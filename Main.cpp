68d79594cf33e4c46ea1e2c94def25b6350d4b8c
Jeffrey Teh <59184460+Jeffderp22@users.noreply.github.com>
2022-03-07 22:25:33 -0800

    Added Random Name generator

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <stdlib.h>
 #include "Student.h"
 
 using namespace std;
@@ -18,6 +19,7 @@ int main(){
 
 Student* generateStudent(){
     char* arr[100];
+    char* arr2[100];
     fstream firstNames;
     firstNames.open("FirstNames.txt");
     for(int i = 0; i<100; i++){
@@ -25,7 +27,14 @@ Student* generateStudent(){
     firstNames >> firstName;
     arr[i] = firstName;
     }
-
     fstream lastNames;
     lastNames.open("LastNames.txt");
+    for(int j = 0; j<100; j++){
+    char* lastName = new char[50];
+    lastNames >> lastName;
+    arr2[j] = lastName;
+    }
+    char* studentFirst = arr[rand() %100 + 1];
+    char* studentFirst = arr[rand() %100 + 1];
+
 }
